331b5d01815442d4224fd7f629d13a25c53edd73
KatherinaLarin <Katherinal4@nycstudents.net>
2025-12-01 16:53:34 -0500

    Making Mad Libs

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main (String[] args){
+        String word1="Opal";
+        String word2="Mars";
+        String word3="People";
+        String word4="cried";
+        int num1=4;
+        System.out.println("Long ago there lived a "+ word1);
+        System.out.print("The "+word1+"s "+word2+" blew up.");
+        System.out.print("The "+word1+" was scared afterwards and lived for "+num1+" months in a hole.");
+        System.out.print("Some "+word3+" wandered into its hole.");
+        System.out.print("The "+word1+" went outside chasing after them and then it "+word4+" for "+num1+ " minutes.");
+    }
+}
+// origString=newString+origString
+
